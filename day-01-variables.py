9e8ca01d83238acef242e2c68373d37d7effb81b
KhadkaRenish <githubrenish@gmail.com>
2026-07-23 20:44:28 +0545

    Complete Day 1 variables and data types practice

--- day-01-variables.py
+++ day-01-variables.py
@@ -1,1 +1,77 @@
-print("hello world")
+"""
+What is a variable?
+A variable is a name that represents stored information.
+
+Think of it like a labeled container.
+
+For example:
+Student name -> Renish
+Student age -> 25
+Exam score -> 82.5
+Passed Exam -> True
+
+"""
+
+
+"""
+Why do we need varibales?
+Without variables, a program cannot easily remember or manage changing information.
+
+Variables are used for things such as:
+Usernames
+Employee IDs
+Price
+Exam scores
+Login attempts
+Account status
+Temperature
+Application names
+
+"""
+
+"""
+The four basic data types 
+String, integer, float, boolean
+
+"""
+
+"""
+Variables naming
+A good varibales name should clearly explain what infromation it stores.
+
+Good names:
+student_name
+exam_score
+employee_status
+monthly_salary
+
+
+Week names:
+x
+data
+thing
+
+Variable naming rules
+Python variable names should follow these rules:
+A name can contain letters, numbers and underscores.
+A name cannot begin with a number.
+Spaces are not allowed.
+Special symbols such as hyphens are generally not used.
+Python treats uppercase and lowercase name as different.
+
+The common Python naming style is called snake_case.
+
+Examples:
+student_name
+total_score
+is_account_active
+
+"""
+
+
+full_name = "Renish Singh Khadka"
+age = 22
+monthly_salary = 333
+is_currently_employed = True
+phone_number = 3242324
+
